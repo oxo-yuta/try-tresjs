e70bc86991a83f0afe1a9a6a5c27bf3fb5135107
Yuta Okuzono <spadeloves@gmail.com>
2023-02-03 22:21:57 +0900

    use tres

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import Tres from '@tresjs/core'
+
+export const app = createApp(App)
+
+app.use(router)
+app.use(Tres)
+app.mount('#app')
